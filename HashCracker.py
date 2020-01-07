dd03eb4b4a2bf93c52b5b2dfca6dee196571738e
jokerhacker22 <53850334+jokerhacker22@users.noreply.github.com>
2020-01-07 12:10:07 -0500

    Add files via upload

---
+++ HashCracker.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#coding: utf-8
+
+import hashlib
+import os 
+
+os.sys("clear")
+flag = 0
+
+print("\033[1;32;40m ░░░░░██╗░█████╗░██╗░░██╗███████╗██████╗░  ██╗░░██╗░█████╗░░█████╗░██╗░░██╗███████╗██████╗░")
+print("\033[1;32;40m ░░░░░██║██╔══██╗██║░██╔╝██╔════╝██╔══██╗  ██║░░██║██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗")
+print("\033[1;32;40m ░░░░░██║██║░░██║█████═╝░█████╗░░██████╔╝  ███████║███████║██║░░╚═╝█████═╝░█████╗░░██████╔╝")
+print("\033[1;32;40m ██╗░░██║██║░░██║██╔═██╗░██╔══╝░░██╔══██╗  ██╔══██║██╔══██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗")
+print("\033[1;32;40m ╚█████╔╝╚█████╔╝██║░╚██╗███████╗██║░░██║  ██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║")
+print("\033[1;32;40m ░╚════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝\n")
+print("                            This Script Was Made By JOKER HACKER​​​​​                          ")
+ 
+                                                                                                                                   
+pass_hash = input("\033[1;36;40m Enter the md5 hash: ")
+wordlist = input("\033[1;36;40m  Wordlis Location: ")
+
+try:
+    pass_file = open (wordlist, "r")
+except:
+    print("No file found")
+    quit()
+
+for word in pass_file:
+
+    enc_wrd = word.encode('utf-8')
+    digest = hashlib.md5(enc_wrd.strip()).hexdigest()
+
+    if digest == pass_hash:
+        print("\tpassword found")
+        print("Password is " + word)
+        flag = 1
+        break
+
+if flag ==0:
+    print("\tPassword is not in the list")
---
+++ README.md
@@ -0,0 +1,14 @@
+This Script Was Made By JOKER HACKER
+
+It was open source and anyone can use this script
+
+#instructions
+
+cd Hash-Cracker
+
+python3 HashCracker.py
+
+#usage
+
+1. Enter the hash string
+2. Then Enter the wordlist location
